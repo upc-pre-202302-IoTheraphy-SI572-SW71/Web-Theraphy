5578e64c17bdb6bb3f88e0b872153e979f8fbbef
LuisPineda05 <89112808+LuisPineda05@users.noreply.github.com>
2023-09-26 10:49:31 -0500

    feat(physiotherapist-list): added searchbar and icon button

---
+++ src/app/appointments/pages/physiotherapist-list/physiotherapist-list.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+
+@Component({
+  selector: 'app-physiotherapist-list',
+  templateUrl: './physiotherapist-list.component.html',
+  styleUrls: ['./physiotherapist-list.component.css'],
+})
+export class PhysiotherapistListComponent {
+  value: any;
+
+}
