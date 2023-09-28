1052d29794540977abe2a7b97743a988a55bc656
GabrielGomezDlc <u202020341@upc.edu.pe>
2023-09-28 02:42:12 -0500

    feat(sidenav): created sidenav

---
+++ src/app/components/sidenav/sidenavMenuData.ts
@@ -0,0 +1,32 @@
+export const sidenavMenuData = [
+  {
+    routeLink: '/home-patient',
+    icon: 'home',
+    label: 'Home'
+  },
+  {
+    routeLink: '/profile-patient',
+    icon: 'account_circle',
+    label: 'Profile'
+  },
+  {
+    routeLink: '/treatments',
+    icon: 'accessibility',
+    label: 'Treatments'
+  },
+  {
+    routeLink: '/physiotherapists',
+    icon: 'group',
+    label: 'Physiotherapists'
+  },
+  {
+    routeLink: '/medical-appointments',
+    icon: 'calendar_today',
+    label: 'Appointments'
+  },
+  {
+    routeLink: '/my-treatments',
+    icon: 'accessibility',
+    label: 'My Treatments'
+  }
+]
