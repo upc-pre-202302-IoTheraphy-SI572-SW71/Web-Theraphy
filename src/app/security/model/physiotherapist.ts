1a6b8639a2b26ec5fbbbffe958b6b258980cd6be
María Alexandra Ahuanari Murayari <89022162+MariaAhuanari@users.noreply.github.com>
2023-09-28 03:02:26 -0500

    feat(security): Created physiotherapist model

---
+++ src/app/security/model/physiotherapist.ts
@@ -0,0 +1,15 @@
+export interface Physiotherapist {
+  id: number,
+  userId:number,
+  firstName: string,
+  paternalSurname: string,
+  maternalSurname: string,
+  specialization: string,
+  age: number,
+  location: string,
+  birthdayDate: string,
+  email: string,
+  rating: number,
+  consultationsQuantity: number,
+  photoUrl: string
+}
