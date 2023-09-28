0f3254a26975fe84459183b5981797e5cd282f8a
María Alexandra Ahuanari Murayari <89022162+MariaAhuanari@users.noreply.github.com>
2023-09-28 03:01:03 -0500

    feat(security): Created entity and services for users

---
+++ src/app/security/model/user.ts
@@ -0,0 +1,11 @@
+export interface User{
+  id: number,
+  username: string,
+  email: string,
+  password: string
+  type: string,
+  firstName: string,
+  paternalSurname: string,
+  maternalSurname: string,
+  birthdayDate: string
+}
