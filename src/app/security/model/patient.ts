67bfdb3b03997b7a8bc36c3edf14497e78eb3804
María Alexandra Ahuanari Murayari <89022162+MariaAhuanari@users.noreply.github.com>
2023-09-28 03:04:25 -0500

    feat(security): Created patient model

---
+++ src/app/security/model/patient.ts
@@ -0,0 +1,12 @@
+export interface Patient {
+  id: number,
+  userId: number,
+  firstName: string,
+  lastName: string,
+  age: number,
+  birthdayDate: string,
+  email: string;
+  appointmentQuantity: number,
+  photoUrl: string,
+  //createdAt: string,
+}
