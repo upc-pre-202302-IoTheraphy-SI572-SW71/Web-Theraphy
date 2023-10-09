f3a89a38b927cc8b313f246ab2d67eb794478bd5
GabrielGomezDlc <u202020341@upc.edu.pe>
2023-10-09 15:42:38 -0500

    feat(physiotherapist-profile): Added physiotherapist profile styles

---
+++ src/app/appointments/pages/physiotherapist-profile/physiotherapist-profile.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-physiotherapist-profile',
+  templateUrl: './physiotherapist-profile.component.html',
+  styleUrls: ['./physiotherapist-profile.component.css']
+})
+export class PhysiotherapistProfileComponent {
+
+}
