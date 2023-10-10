c7914cb56f1bab0f102e8aabdaffe200345389c4
GabrielGomezDlc <u202020341@upc.edu.pe>
2023-10-10 11:41:25 -0500

    fix: Fixed app-routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -10,10 +10,25 @@ import {
 import {
   PhysiotherapistListComponent
 } from "./therapy-coordination/pages/physiotherapist-list/physiotherapist-list.component";
+import {LoginComponent} from "./security/pages/login-in/login.component";
+import {SignupComponent} from "./security/pages/signup/signup.component";
+import {PatientRegisterComponent} from "./security/pages/patient-register/patient-register.component";
+import {
+  PhysiotherapistRegisterComponent
+} from "./security/pages/physiotherapist-register/physiotherapist-register.component";
+import {BookConsultationComponent} from "./appointments/pages/book-consultation/book-consultation.component";
 
 const routes: Routes = [
+  {path: '' , component: LoginComponent},
+  {path: 'auth-registration', component: SignupComponent},
+  {path: 'registration-patient', component: PatientRegisterComponent},
+  {path: 'registration-physiotherapist', component: PhysiotherapistRegisterComponent},
+
   {path: 'physiotherapist-list', component: PhysiotherapistListComponent},
-  {path: 'physiotherapist-profile/:id', component: PhysiotherapistProfileComponent}
+  {path: 'physiotherapist-profile/:id', component: PhysiotherapistProfileComponent},
+  {path: 'book-consultation', component: BookConsultationComponent},
+
+
 
 ];
 
