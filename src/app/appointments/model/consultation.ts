7bfe65fe6d880acde7811f765fbe00ccd3eb1ac8
GabrielGomezDlc <u202020341@upc.edu.pe>
2023-10-10 16:52:15 -0500

    fix: fixed consultation model

---
+++ src/app/appointments/model/consultation.ts
@@ -0,0 +1,10 @@
+export interface Consultation{
+  id: number;
+  done: boolean;
+  topic: string;
+  diagnosis: string;
+  date: string;
+  hour: string;
+  place: string;
+  physiotherapistId: number;
+}
