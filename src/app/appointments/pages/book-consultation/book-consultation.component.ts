6c97fc77a920b38ef3e9675aced7f73209e80a5f
leona <u202018777@upc.edu.pe>
2023-10-10 21:52:04 -0500

    feat(BookConsultation): fix styles to classes in html.

--- src/app/appointments/pages/book-consultation/book-consultation.component.ts
+++ src/app/appointments/pages/book-consultation/book-consultation.component.ts
@@ -10,28 +10,26 @@ export class BookConsultationComponent {
   hours: number[] = Array.from({ length: 13 }, (_, i) => i + 7); // Array de horas (0-23)
   minutes: number[] = [0, 15, 20, 30, 35, 45, 50]; // Array de minutos (0-59)
 
-  selectedHour: number = 7; // Hora seleccionada inicialmente
+  selectedHour: number = 7;
   selectedMinute: number = 0;
 
   selectedDate: Date | null = null;
 
   hourSelected: boolean = false;
 
-  selectedPeriod: string = ""; // Inicialmente vacío
+  selectedPeriod: string = "";
 
-// ...
 
-// Función para determinar "AM" o "PM" según la hora seleccionada
   setPeriod() {
     this.selectedPeriod = this.selectedHour < 12 ? "AM" : "PM";
   }
 
   onDateChange(selectedDate: Date): void {
-    // Verifica si la fecha seleccionada es un domingo (0 = Domingo, 1 = Lunes, ..., 6 = Sábado)
-    if (selectedDate && selectedDate.getDay() === 0) {
-      // Restablece la fecha seleccionada o muestra un mensaje de error
-      this.selectedDate = null;
 
+    if (selectedDate && selectedDate.getDay() === 0) {
+
+      this.selectedDate = null;
+        //to do
       // if(si esta 0 no podra hacer cita )
       console.log('No puedes seleccionar un domingo.');
     }
