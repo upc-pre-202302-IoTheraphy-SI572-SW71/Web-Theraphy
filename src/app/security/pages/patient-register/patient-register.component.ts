278e379438609941b677a51eb1532bdd53a10b90
delca <u202020239@upc.edu.pe>
2023-10-13 00:17:52 -0500

    feat(update): Update patient-register html, ts and css

--- src/app/security/pages/patient-register/patient-register.component.ts
+++ src/app/security/pages/patient-register/patient-register.component.ts
@@ -2,8 +2,7 @@ import { Component } from '@angular/core';
 import {Patient} from "../../model/patient";
 import {PatientService} from "../../services/patient.service";
 import {Router} from "@angular/router";
-import {FormBuilder, FormGroup, Validators} from "@angular/forms";
-import {DatePipe} from "@angular/common";
+import {FormBuilder, FormControl, FormGroup, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-patient-register',
@@ -15,35 +14,45 @@ export class PatientRegisterComponent {
     0,
     '',
     0,
-    'dasd',
+    'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png',
     '',
     0,
     ''
   );
-
   constructor(
     private formBuilder: FormBuilder,
     private patientService: PatientService,
     private router: Router,
-    private datePipe: DatePipe
   ) {}
 
   submitPatientForm() {
-
-    this.patientService.createPatient(this.patient).subscribe(
-      () => {
-        this.router.navigate(['/physiotherapist-list']);
-      },
-      (error) => {
-        console.error('Error al crear paciente:', error);
+    if (this.registerForm && this.registerForm.get('birthdayDate')) {
+      const birthdayDate = this.registerForm.get('birthdayDate')!.value;
+      if (birthdayDate) {
+        const formattedBirthday = this.formatDate(birthdayDate);
+        this.patient.birthdayDate = formattedBirthday;
+        this.patientService.createPatient(this.patient).subscribe(
+          () => {
+            this.router.navigate(['/physiotherapist-list']);
+          },
+          (error) => {
+            console.error('Error al crear paciente:', error);
+          }
+        );
       }
-    );
+    }
   }
-
   registerForm: FormGroup = this.formBuilder.group({
-    dni: ['', {validators: [Validators.required], updatedOn: 'change'}],
+    dni: ['', {validators: [Validators.required,Validators.pattern(/^\d{8}$/)], updatedOn: 'change'}],
     age: ['', {validators: [Validators.required], updatedOn: 'change'}],
     birthdayDate: ['', {validators: [Validators.required], updatedOn: 'change'}],
     location: ['', {validators: [Validators.required], updatedOn: 'change'}]
   })
+  formatDate(date: string): string {
+    const dateObj = new Date(date);
+    const year = dateObj.getFullYear();
+    const month = (dateObj.getMonth() + 1).toString().padStart(2, '0');
+    const day = dateObj.getDate().toString().padStart(2, '0');
+    return `${year}-${month}-${day}`;
+  }
 }
