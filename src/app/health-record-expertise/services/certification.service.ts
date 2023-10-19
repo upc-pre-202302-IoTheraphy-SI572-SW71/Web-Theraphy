0f53afe05b2082ecccdc7804372f34cc4850ad8f
GabrielGomezDlc <u202020341@upc.edu.pe>
2023-10-18 21:55:02 -0500

    feat(physiotherapist-profile): added certifications services

---
+++ src/app/health-record-expertise/services/certification.service.ts
@@ -0,0 +1,36 @@
+import { Injectable } from '@angular/core';
+import {BaseService} from "../../shared/services/base.service";
+import {Job} from "../model/job";
+import {HttpClient, HttpHeaders} from "@angular/common/http";
+import {catchError, Observable, retry} from "rxjs";
+import {Certification} from "../model/certification";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CertificationService extends BaseService<Certification>{
+
+  endPoint = '/certifications';
+
+  constructor(http: HttpClient) {
+    super(http);
+    this.basePath += this.endPoint;
+  }
+
+  getByPhysiotherapistId(physiotherapistId: number): Observable<Certification> {
+    const getCertificationsByPhysiotherapistIdUrl = `${this.basePath}/byPhysiotherapistId/${physiotherapistId}`;
+    const jwtToken = localStorage.getItem('jwtToken');
+
+    if (!jwtToken) {
+      throw new Error('Token JWT no encontrado en el localStorage.');
+    }
+    const headers = new HttpHeaders({
+      'Authorization': `Bearer ${jwtToken}`
+    });
+
+    return this.http.get<Certification>(getCertificationsByPhysiotherapistIdUrl, { headers })
+      .pipe(
+        retry(2),
+        catchError(this.handleError));
+  }
+}
