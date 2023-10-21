abc4e6304b28172535b81e680d7830432c737676
María Alexandra Ahuanari Murayari <U20201A836@UPC.EDU.PE>
2023-10-20 23:35:17 -0500

    feat(home-services): modified consultation services

--- src/app/consultations/services/consultation.service.ts
+++ src/app/consultations/services/consultation.service.ts
@@ -2,8 +2,9 @@ import { Injectable } from '@angular/core';
 import {BaseService} from "../../shared/services/base.service";
 import {CreateConsultation} from "../model/createConsultation";
 import {HttpClient, HttpHeaders} from "@angular/common/http";
-import {Observable} from "rxjs";
+import {catchError, Observable, retry} from "rxjs";
 import {Consultation} from "../model/Consultation";
+
 
 
 
@@ -37,6 +38,22 @@ export class ConsultationService extends BaseService<Consultation>{
 
   }
 
+  getByPatientId(patientId: number): Observable<Consultation> {
+    const getLastConsultationByPatientIdUrl = `${this.basePath}/byPatientId/${patientId}`;
+    const jwtToken = localStorage.getItem('jwtToken');
+
+    if (!jwtToken) {
+      throw new Error('Token JWT no encontrado en el localStorage.');
+    }
+    const headers = new HttpHeaders({
+      'Authorization': `Bearer ${jwtToken}`
+    });
+
+    return this.http.get<Consultation>(getLastConsultationByPatientIdUrl, { headers })
+      .pipe(
+        retry(2),
+        catchError(this.handleError));
+  }
 
 
 }
